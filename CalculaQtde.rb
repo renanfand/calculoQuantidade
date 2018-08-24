4ac1b57e55e3a97f8acaa99cba950a1716ac989e
Renan Andrade <renanfand@gmail.com>
2018-08-24 20:05:37 -0300

    Criação do teste e finalização do desenvolvimento

--- CalculaQtde.rb
+++ CalculaQtde.rb
@@ -6,6 +6,19 @@ class CalculaQtde
 	end
 
 	def calculoFumante
-		puts ((anosFumando*365) * (nCigarrosDia*(valorCarteira/10))));
+		puts ((@anosFumando*365) * (@nCigarrosDia*(@valorCarteira/10))));
 	end
 end
+
+puts "Informe a quantidade de anos funmando"
+anosFumando = gets.chomp.to_i
+
+puts "Infome o numero de cigarros fumado por dia"
+nCigarrosDia = gets.chomp.to_i
+
+puts "Informe o valor da cateira de cigarro"
+valorCarteira = gets.chomp.to_f
+
+
+calc = CalculaQtde.new(anosFumando, nCigarrosDia, valorCarteira)
+calc.calculoFumante
