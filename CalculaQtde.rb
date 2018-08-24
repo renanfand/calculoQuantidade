9b3494f11e832d5ed8fa970ef528c4b90f9f9bdb
Renan Andrade <renanfand@gmail.com>
2018-08-24 19:58:13 -0300

    Criação do metodo para o calculo

--- CalculaQtde.rb
+++ CalculaQtde.rb
@@ -4,5 +4,8 @@ class CalculaQtde
 	def initialize(anosFumando, nCigarrosDia, valorCarteira)
 		@anosFumando, @nCigarrosDia, valorCarteira, = anosFumando, nCigarrosDia, valorCarteira
 	end
-	
+
+	def calculoFumante
+		puts ((anosFumando*365) * (nCigarrosDia*(valorCarteira/10))));
+	end
 end
